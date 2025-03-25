70f985cd42e18cf96b153c793bed6ad49835ac45
MUTHUGANESH <115532315+MUTHUGANESH13@users.noreply.github.com>
2025-03-25 13:59:40 +0530

    Add files via upload

---
+++ JAVA/2870. Minimum Number of Operations to Make Array Empty.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minOperations(int[] n) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int task : n)
+            map.put(task, map.getOrDefault(task, 0) + 1);
+        for (int freq : map.values()) {
+            while (freq != 0) {
+                if (freq == 1)
+                    return -1;
+                else if (freq == 4) {
+                    count += 2;
+                    freq -= 4;
+                } else if (freq == 2) {
+                    count++;
+                    freq -= 2;
+                } else {
+                    count++;
+                    freq -= 3;
+                }
+            }
+        }
+        return count;
+    }
+}
